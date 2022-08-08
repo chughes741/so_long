824910b221cca9487d508d40e1b057f7ddd8fdf5
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-08-08 12:09:24 -0400

    Added events header

--- include/events.h
+++ include/events.h
@@ -13,4 +13,14 @@
 #ifndef EVENTS_H
 # define EVENTS_H
 
+enum {
+	ON_KEYDOWN = 2,
+	ON_KEYUP = 3,
+	ON_MOUSEDOWN = 4,
+	ON_MOUSEUP = 5,
+	ON_MOUSEMOVE = 6,
+	ON_EXPOSE = 12,
+	ON_DESTROY = 17
+};
+
 #endif
