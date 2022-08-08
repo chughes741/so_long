1c8384d82f1d5241e95c30f1f1d53bb82cba2926
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-08-08 12:09:07 -0400

    Added singleton constructor for t_data

---
+++ src/get_data.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_data.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chughes <chughes@42quebec.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/23 13:33:54 by chughes           #+#    #+#             */
+/*   Updated: 2022/06/23 13:37:44 by chughes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/so_long.h"
+
+// Singleton itialization and reference getter
+t_data	*get_data(void)
+{
+	static t_data* data = NULL;
+
+	if (data == NULL)
+		data = ft_calloc(1, sizeof(t_data));
+	return (data);
+}
