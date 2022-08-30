f063c58711a0699db550a02e022a5817296ea4c2
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-08-30 15:21:35 -0400

    Moved exit_error to file

--- src/exit_error.c
+++ src/exit_error.c
@@ -12,42 +12,13 @@
 
 #include "../include/so_long.h"
 
-// Singleton itialization and reference getter
-t_data	*get_data(void)
-{
-	static t_data	*data = NULL;
-
-	if (data == NULL)
-		data = ft_calloc(1, sizeof(t_data));
-	return (data);
-}
-
-// Initialized data struct
-void	init_data(int argc, char *argv[])
-{
-	t_data	*d;
-	int		w;
-	int		h;
-
-	d = get_data();
-	check_input(argc, argv);
-	d->img = ft_calloc(1, sizeof(t_img *));
-	d->img->wall = mlx_xpm_file_to_image(d->mlx, "./assets/1.xpm", &w, &h);
-	d->img->empty = mlx_xpm_file_to_image(d->mlx, "./assets/0.xpm", &w, &h);
-	d->img->exit = mlx_xpm_file_to_image(d->mlx, "./assets/E.xpm", &w, &h);
-	d->img->collec = mlx_xpm_file_to_image(d->mlx, "./assets/C.xpm", &w, &h);
-	d->img->charac = mlx_xpm_file_to_image(d->mlx, "./assets/P.xpm", &w, &h);
-	return ;
-}
-
-// Frees data and its contents
-void	del_data(void)
+// Called to exit program and display error
+void	exit_error(void)
 {
 	t_data	*data;
 
 	data = get_data();
-	if (data->img)
-		free(data->img);
-	free(data);
-	return ;
+	del_data();
+	perror("Error\n");
+	exit(1);
 }
