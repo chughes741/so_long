5273482970bfe7ea838ff16132c61213f3e9a6fd
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-08-31 18:34:29 -0400

    Removed test code

--- src/so_long.c
+++ src/so_long.c
@@ -20,8 +20,6 @@ int	main(int argc, char *argv[])
 	d->mlx = mlx_init();
 	init_data(argc, argv);
 	d->win = mlx_new_window(d->mlx, d->width * 64, d->height * 64, "so_long");
-	// for (int i = 0; i < d->height; ++i)
-		// printf("%s", d->map[i]);
 	mlx_hook(d->win, ON_DESTROY, 0, exit_window, &d);
 	mlx_hook(d->win, ON_KEYDOWN, 0, keydown, &d);
 	mlx_loop_hook(d->mlx, render_frame, NULL);
